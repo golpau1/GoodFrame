b0f24197651cafbe94c97f6032d21f988a0b00fa
golpau1 <loganwallace049@gmail.com>
2026-08-10 10:15:46 +1000

    Replace header logo artwork

--- Shared/site-header.js
+++ Shared/site-header.js
@@ -15,8 +15,8 @@
 
     <div class="secondary-header site-navigation">
       <a href="${href("index.html")}" class="header-title header-title--logo" aria-label="Good Frame home">
-        <svg class="header-logo" viewBox="333 1534 5392 770" aria-hidden="true" focusable="false">
-          <image href="${href("Assets/good-frame-logo.png")}" width="6000" height="4000"></image>
+        <svg class="header-logo" viewBox="333 1534 5450 770" aria-hidden="true" focusable="false">
+          <image href="${href("Assets/good-frame-logo.png?v=20260810-v2")}" width="6000" height="4000"></image>
         </svg>
       </a>
 
